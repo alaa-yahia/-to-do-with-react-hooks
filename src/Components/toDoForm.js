97b0c229341a9425bf034fadcd157c3bd4f68311
Alaa Yahia <alaay873@live.com>
2020-01-23 14:38:24 +0200

    insert new item

--- src/Components/toDoForm.js
+++ src/Components/toDoForm.js
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
 
-function ToDoForm () {
- return (
-    <div>
+function ToDoForm ({insertTodo, todoListLength}) {
+    const [inputValue, setInputValue] = useState('');
 
+    function handleChange(e) {
+        const value = e.target.value;
+        setInputValue(value);
+    }
+
+    function handleClick() {
+        if(inputValue) {
+            const item = {todo: inputValue, checked: false, key: todoListLength};
+            insertTodo(item);
+            setInputValue('');
+        }
+    }
+
+    return (
+    <div className="form">
+        <input type="text" onChange={handleChange} value={inputValue} />
+        <button onClick={handleClick}>Add</button>
     </div>
  );
 }
